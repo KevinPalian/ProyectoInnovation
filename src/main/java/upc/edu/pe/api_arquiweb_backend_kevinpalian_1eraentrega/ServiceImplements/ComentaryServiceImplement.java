2de1293f9364596050bad99c75833a8e210be453
Arturo-Jesus7 <u202211388@upc.edu.pe>
2025-04-29 22:28:10 -0500

    Nuevas tablas con nombres y datos en ingles

--- src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/ServiceImplements/ComentaryServiceImplement.java
+++ src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/ServiceImplements/ComentaryServiceImplement.java
@@ -1,10 +1,10 @@
-package pe.edu.upc.demo3157api.servicesimplements;
+package upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.ServiceImplements;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pe.edu.upc.demo3157api.entities.Comentary;
-import pe.edu.upc.demo3157api.repositories.IComentaryRepository;
-import pe.edu.upc.demo3157api.servicesinterfaces.IComentaryService;
+import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.entities.Comentary;
+import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.repositories.IComentaryRepository;
+import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.ServicesInterface.IComentaryService;
 
 import java.util.List;
 @Service
