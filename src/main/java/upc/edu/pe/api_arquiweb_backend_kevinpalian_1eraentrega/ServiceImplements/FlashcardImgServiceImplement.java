219aca71983f6ef6b6d17d39e0c91219ac992ff5
Arturo-Jesus7 <u202211388@upc.edu.pe>
2025-04-29 22:29:34 -0500

    Nuevas tablas con nombres y datos en ingles

--- src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/ServiceImplements/FlashcardImgServiceImplement.java
+++ src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/ServiceImplements/FlashcardImgServiceImplement.java
@@ -1,10 +1,10 @@
-package pe.edu.upc.demo3157api.servicesimplements;
+package upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.ServiceImplements;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pe.edu.upc.demo3157api.entities.FlashcardImg;
-import pe.edu.upc.demo3157api.repositories.IFlashcardImgRepository;
-import pe.edu.upc.demo3157api.servicesinterfaces.IFlashcardImgService;
+import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.entities.FlashcardImg;
+import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.repositories.IFlashcardImgRepository;
+import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.ServicesInterface.IFlashcardImgService;
 
 import java.util.List;
 @Service
