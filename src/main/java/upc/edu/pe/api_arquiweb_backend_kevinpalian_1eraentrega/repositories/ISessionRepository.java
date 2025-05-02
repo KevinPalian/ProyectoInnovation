4d97eedefd6b71d9ca8a52f528fa53762f86daf2
GerardTempladera <u20221440@upc.edu.pe>
2025-05-02 00:42:37 -0500

    Actualice mi tabla

--- src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/repositories/ISessionRepository.java
+++ src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/repositories/ISessionRepository.java
@@ -3,10 +3,10 @@ package upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.entities.Sesion;
+import upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.entities.Session;
 
 @Repository
-public interface ISesionRepository extends JpaRepository<Sesion, Integer> {
+public interface ISessionRepository extends JpaRepository<Session, Integer> {
 
 
 
