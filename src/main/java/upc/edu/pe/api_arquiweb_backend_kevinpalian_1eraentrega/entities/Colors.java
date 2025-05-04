be90915af93f9d50ba2afe418a49143900d4707f
KevinPalian <u20211c594@upc.edu.pe>
2025-05-04 01:04:09 -0500

    Agregue un comentario

--- src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/entities/Colors.java
+++ src/main/java/upc/edu/pe/api_arquiweb_backend_kevinpalian_1eraentrega/entities/Colors.java
@@ -3,7 +3,7 @@ package upc.edu.pe.api_arquiweb_backend_kevinpalian_1eraentrega.entities;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Colores")
+@Table(name = "Colores") //Esto es un cambio
 public class Colors {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
